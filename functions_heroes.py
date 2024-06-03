95d24ceb7f5c3b8ba6aac5c6a1226611704b63e7
JAGLeon <anthonygarayleon@gmail.com>
2024-06-03 15:51:58 -0300

    funciones especificas

--- functions_heroes.py
+++ functions_heroes.py
@@ -1,55 +1,55 @@
-def mostrar_lista_campos(lista,campos):
+def mostrar_lista_campos(lista:list,campos:list):
     print()
     for elem in lista:
        mostrar_elemento_por_campos(elem, campos) 
        print()
 
-def mostrar_elemento_por_campos(elemento, campos):
+def mostrar_elemento_por_campos(elemento:dict, campos:list):
     print()
     for campo in campos:
         valor = elemento.get(campo, "No existe")
         print(f"{valor}", end = " ")
 
-def mostrar_dictionary(dict):
+def mostrar_dictionary(dict:dict):
     for x , y in dict.items():
         print(f"{x} => {y}")
 
-def mostrar_dictionary_lista_campos(dict,campos):
+def mostrar_dictionary_lista_campos(dict:dict,campos:list):
     for clave, listas in dict.items():
         print(f"\nclave {clave} :")
         mostrar_lista_campos(listas,campos)
 
-def filtrar_lista(funct,lista):
+def filtrar_lista(funct:function,lista:list)->list:
     lista_filtrada = []
     for elem in lista:
         if funct(elem):
             lista_filtrada.append(elem)
     return lista_filtrada
 
-def filtrar_atributo(atributo,lista):
+def filtrar_atributo(clave:str,lista:list)->list:
     lista_filtrada = []
     for elem in lista:
-        lista_filtrada.append(elem[atributo])
+        lista_filtrada.append(elem[clave])
     return lista_filtrada
 
-def comprar_items(fun, lista):     
+def comprar_items(funct:function, lista:list)->dict:     
     bandera = True     
     for i in lista:         
-        if bandera or fun(i, item):             
+        if bandera or funct(i, item):             
             bandera = False
             item = i
     return item
 
-def sumar_lista(lista):
+def sumar_lista(lista:list)->float:
     suma = 0
     for elem in lista:
         suma += float(elem)
     return suma
 
-def promedio_lista(lista):
+def promedio_lista(lista:list)->float:
     return sumar_lista(lista) / len(lista)
     
-def agrupar_elementos(clave,lista):
+def agrupar_elementos(clave:str,lista:list)->dict:
     diccionario = {}
     for elem in lista:
         cam_rep = elem.get(clave, "No existe")
@@ -62,70 +62,57 @@ def agrupar_elementos(clave,lista):
             diccionario[cam_rep] = [elem]
     return diccionario
 
-######################Funciones Especificas#######################
-def lista_masculinos(lista):
-    lista_heroes = filtrar_lista(lambda heroe: heroe["genero"] == "M",lista)
-    return lista_heroes
+def contar_elementos(clave:str,lista:list)->dict:
+    dic_new = {}
+    dic = agrupar_elementos(clave,lista) 
+    for c in dic:
+        dic_new[c] = len(dic[c])
+    return dic_new
 
-def lista_femeninos(lista):
+######################Funciones Especificas#######################
+
+def lista_masculinos(lista:list)->list:
+    return filtrar_lista(lambda heroe: heroe["genero"] == "M",lista)
+
+def lista_femeninos(lista:list)->list:
     return filtrar_lista(lambda heroe: heroe["genero"] == "F",lista)
 
-def heroe_alto(lista):
-    lista_heroes = lista_masculinos(lista)
-    return comprar_items(lambda h1,h2: float(h1["altura"]) > float(h2["altura"]),lista_heroes)
+def heroe_alto(lista:list)->dict:
+    return comprar_items(lambda h1,h2: float(h1["altura"]) > float(h2["altura"]),lista_masculinos(lista))
 
-def heroe_bajo(lista):
-    lista_heroes = lista_masculinos(lista)
-    return comprar_items(lambda h1,h2: float(h1["altura"]) < float(h2["altura"]),lista_heroes)
+def heroe_bajo(lista:list)->dict:
+    return comprar_items(lambda h1,h2: float(h1["altura"]) < float(h2["altura"]),lista_masculinos(lista))
 
-def heroina_alta(lista):
-    lista_heroinas = lista_femeninos(lista)
-    return comprar_items(lambda h1,h2: float(h1["altura"]) > float(h2["altura"]),lista_heroinas)
+def heroina_alta(lista:list)->dict:
+    return comprar_items(lambda h1,h2: float(h1["altura"]) > float(h2["altura"]),lista_femeninos(lista))
 
-def heroina_baja(lista):
-    lista_heroinas = lista_femeninos(lista)
-    return comprar_items(lambda h1,h2: float(h1["altura"]) < float(h2["altura"]),lista_heroinas)
+def heroina_baja(lista:list)->dict:
+    return comprar_items(lambda h1,h2: float(h1["altura"]) < float(h2["altura"]),lista_femeninos(lista))
 
-def promedio_altura_masculinos(lista):
-    lista_heroes = lista_masculinos(lista)
-    lista_alturas = filtrar_atributo("altura",lista_heroes)
-    return promedio_lista(lista_alturas)
+def promedio_altura_masculinos(lista:list)->float:
+    return promedio_lista(filtrar_atributo("altura",lista_masculinos(lista)))
 
-def promedio_altura_femeninos(lista):
-    lista_heroinas = lista_femeninos(lista)
-    lista_alturas = filtrar_atributo("altura",lista_heroinas)
-    return promedio_lista(lista_alturas)
+def promedio_altura_femeninos(lista:list)->float:
+    return promedio_lista(filtrar_atributo("altura",lista_femeninos(lista)))
 
-def supers_altos_bajos(lista):
+def supers_altos_bajos(lista:list)->list:
     return [heroe_alto(lista),heroina_alta(lista),heroe_bajo(lista),heroina_baja(lista)]
 
-def color_ojos_repetidos(lista):
-    dic_new = {}
-    dic = agrupar_elementos("color_ojos",lista) 
-    for clave in dic:
-        dic_new[clave] = len(dic[clave])
-    return dic_new
+def color_ojos_repetidos(lista:list)->dict:
+    return contar_elementos("color_ojos",lista)
 
-def color_pelo_repetidos(lista):
-    dic_new = {}
-    dic = agrupar_elementos("color_pelo",lista) 
-    for clave in dic:
-        dic_new[clave] = len(dic[clave])
-    return dic_new
+def color_pelo_repetidos(lista:list)->dict:
+    return contar_elementos("color_pelo",lista)
 
-def inteligencia_repetidos(lista):
-    dic_new = {}
-    dic = agrupar_elementos("inteligencia",lista) 
-    for clave in dic:
-        dic_new[clave] = len(dic[clave])
-    return dic_new
+def inteligencia_repetidos(lista:list)->dict:
+    return contar_elementos("inteligencia",lista)
 
-def supers_color_ojos(lista):
+def supers_color_ojos(lista:list)->dict:
     return agrupar_elementos("color_ojos",lista)
 
-def supers_color_pelo(lista):
+def supers_color_pelo(lista:list)->dict:
     return agrupar_elementos("color_pelo",lista)
 
-def supers_inteligencia(lista):
+def supers_inteligencia(lista:list)->dict:
     return agrupar_elementos("inteligencia",lista)
 
