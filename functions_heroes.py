bb0c16310b6b441f6b23829fd92a3b842523774f
JAGLeon <anthonygarayleon@gmail.com>
2024-06-07 22:25:50 -0300

    cositas

--- functions_heroes.py
+++ functions_heroes.py
@@ -1,8 +1,42 @@
+from typing import Callable
+def validar_lista(lista:list):
+    '''
+        Recibe una lista, en caso de no ser tipo list da un TypeError
+    '''
+
+    if not isinstance(lista,list):
+        raise TypeError("Se esperaba una lista")
+    
+def validar_diccionario(diccionario:dict):
+    '''
+        Recibe una diccionario, en caso de no ser tipo dict da un TypeError
+    '''
+
+    if not isinstance(diccionario,dict):
+        raise TypeError("Se esperaba una diccionario")
+
+def validar_funcion(funct:Callable):
+    '''
+        Recibe una función lambda o una función regular, en caso de no ser tipo dict da un TypeError
+    '''
+    if not callable(funct):
+        raise ValueError('El argumento proporcionado no es una función.')
+
 def mostrar_lista_campos(lista:list,campos:list):
-    print()
-    for elem in lista:
-       mostrar_elemento_por_campos(elem, campos) 
-       print()
+    '''
+        Recibe una lista
+        Imprime en terminal la lista
+        Caso contrario da un mensaje de error
+    '''
+
+    validar_lista(lista)
+
+    if(len(lista) > 0):
+        for elem in lista:
+            mostrar_elemento_por_campos(elem, campos) 
+            print()
+    else:
+        print('ERROR: La lista no puede ser recorrida')
 
 def mostrar_elemento_por_campos(elemento:dict, campos:list):
     print()
@@ -10,21 +44,84 @@ def mostrar_elemento_por_campos(elemento:dict, campos:list):
         valor = elemento.get(campo, "No existe")
         print(f"{valor}", end = " ")
 
-def mostrar_dictionary(dict:dict):
-    for x , y in dict.items():
-        print(f"{x} => {y}")
+def mostrar_dictionary(diccionario:dict):
+    '''
+        Recibe un diccionario
+        Imprime en terminal el diccionario
+        Caso contrario da un mensaje de error
+    '''
 
-def mostrar_dictionary_lista_campos(dict:dict,campos:list):
-    for clave, listas in dict.items():
-        print(f"\nclave {clave} :")
-        mostrar_lista_campos(listas,campos)
+    validar_diccionario(diccionario)
+    if len(diccionario) > 0:
+        for clave , valor in diccionario.items():
+            print(f"{clave} => {valor}")
+    else: 
+        print('ERROR: El diccionario no puede ser recorrido')
 
-def filtrar_lista(funct:function,lista:list)->list:
+
+def mostrar_dictionary_lista_campos(diccionario:dict,campos:list):
+    '''
+        Recibe un diccionario con valor lista
+        Imprime en terminal el diccionario
+        Caso contrario da un mensaje de error
+    '''
+
+    validar_diccionario(diccionario)
+    if len(diccionario) > 0:
+        for clave, listas in dict.items():
+            print(f"\nclave {clave} :")
+            mostrar_lista_campos(listas,campos)
+    else: 
+        print('ERROR: El diccionario no puede ser recorrido')
+
+
+def filtrar_lista(funct:Callable,lista:list)->list:
+    '''
+        Recibe una lista,funcion
+        Filtra la lista segun la criterio que se envie en funct
+        Caso contrario devuelve una lista vacia
+    '''
+
+    validar_lista(lista)
     lista_filtrada = []
-    for elem in lista:
-        if funct(elem):
-            lista_filtrada.append(elem)
+
+    if(len(lista) > 0):
+        for elem in lista:
+            if funct(elem):
+                lista_filtrada.append(elem)
+
     return lista_filtrada
+#Map hace algo por cada elemento, recibe funct
+def mapear_lista(funct:Callable,lista:list)->list:
+    '''
+        Recibe una lista,funcion
+        Mapea la lista segun la criterio que se envie en funct
+        Caso contrario devuelve una lista vacia
+    '''
+
+    validar_lista(lista)
+    lista_filtrada = []
+
+    if(len(lista) > 0):
+        for elem in lista:
+            lista_filtrada.append(funct(elem))
+
+    return lista_filtrada
+
+
+#Each modifica la lista original, recibe funct
+def each_lista(funct:Callable,clave,lista:list)->list:
+    '''
+        Recibe una lista,funcion
+        Modifica la lista original segun el criterio que se envie en funct
+        Caso contrario devuelve una lista vacia
+    '''
+
+    validar_lista(lista)
+    if(len(lista) > 0):
+        for indice in range(len(lista)):
+            lista[indice][clave] = funct(lista[indice][clave])
+
 
 def filtrar_atributo(clave:str,lista:list)->list:
     lista_filtrada = []
@@ -32,41 +129,73 @@ def filtrar_atributo(clave:str,lista:list)->list:
         lista_filtrada.append(elem[clave])
     return lista_filtrada
 
-def comprar_items(funct:function, lista:list)->dict:     
-    bandera = True     
-    for i in lista:         
-        if bandera or funct(i, item):             
-            bandera = False
-            item = i
-    return item
+def comprar_items(funct, lista:list)->dict:   
+    '''
+        Recibe una lista de la cual filtra dependiendo del lamba
+        Retorna un elemento
+    '''  
+    validar_lista(lista)
+    if(len(lista) > 0):
+        bandera = True     
+        for i in lista:         
+            if bandera or funct(i, item):             
+                bandera = False
+                item = i
+        return item
+    return None
 
 def sumar_lista(lista:list)->float:
+    '''
+        Recibe una lista suma sus elementos
+        Retorna la suma
+    '''  
+    validar_lista(lista)
     suma = 0
-    for elem in lista:
-        suma += float(elem)
+
+    if(len(lista) > 0):
+        for elem in lista:
+            suma += float(elem)
     return suma
 
 def promedio_lista(lista:list)->float:
-    return sumar_lista(lista) / len(lista)
-    
+    '''
+        Recibe una lista suma sus elementos y saca el promedio
+        Retorna el promedio
+    '''  
+    validar_lista(lista)
+    if(len(lista) > 0):
+        return sumar_lista(lista) / len(lista)
+    return None
+
 def agrupar_elementos(clave:str,lista:list)->dict:
+    '''
+        Recibe una lista con diccionarios agrupa en un diccionario segun su valor
+        Retorna un diccionario
+    '''  
+    validar_lista(lista)
     diccionario = {}
-    for elem in lista:
-        cam_rep = elem.get(clave, "No existe")
-        if cam_rep == "":
-            cam_rep = "No Tiene"
-            
-        if cam_rep in diccionario:
-            diccionario[cam_rep].append(elem)
-        else:
-            diccionario[cam_rep] = [elem]
+    if(len(lista) > 0):
+        for elem in lista:
+            cam_rep = elem.get(clave, "No existe")
+            if cam_rep == "":
+                cam_rep = "No Tiene"
+                
+            if cam_rep in diccionario:
+                diccionario[cam_rep].append(elem)
+            else:
+                diccionario[cam_rep] = [elem]
     return diccionario
 
 def contar_elementos(clave:str,lista:list)->dict:
+    '''
+        Recibe una lista con diccionarios agrupa en un diccionario segun su valor 
+        Retorna un diccionario con las veces que se repitio ese valor
+    '''  
     dic_new = {}
-    dic = agrupar_elementos(clave,lista) 
-    for c in dic:
-        dic_new[c] = len(dic[c])
+    if(len(lista) > 0):
+        dic = agrupar_elementos(clave,lista) 
+        for c in dic:
+            dic_new[c] = len(dic[c])
     return dic_new
 
 ######################Funciones Especificas#######################
